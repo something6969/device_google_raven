4dbcce03554faf308b3e17f48ea72d83bf54e8f4
noobyysauraj <gitsauraj@gmail.com>
2022-04-26 18:02:06 +0000

    raven-user Tiramisu TPB1.220310.029 8473704 release-keys
    Signed-off-by: noobyysauraj <gitsauraj@gmail.com>

---
+++ lineage_raven.mk
@@ -0,0 +1,23 @@
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Lineage stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit from raven device
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+PRODUCT_BRAND := google
+PRODUCT_DEVICE := raven
+PRODUCT_MANUFACTURER := google
+PRODUCT_NAME := lineage_raven
+PRODUCT_MODEL := Pixel 6 Pro
+
+PRODUCT_GMS_CLIENTID_BASE := android-google
+TARGET_VENDOR := google
+TARGET_VENDOR_PRODUCT_NAME := raven
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="raven-user Tiramisu TPB1.220310.029 8473704 release-keys"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := google/raven/raven:Tiramisu/TPB1.220310.029/8473704:user/release-keys
